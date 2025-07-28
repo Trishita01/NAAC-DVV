35561cdacff7f7f86441219560f98da6bf9fb5f8
sourick04 <sourickbanerjee@gmail.com>
2025-07-28 17:00:09 +0530

    done

--- backend/src/routes/scores.routes.js
+++ backend/src/routes/scores.routes.js
@@ -1,9 +1,24 @@
 import express from 'express';
 const router = express.Router();
-import { grade211 } from '../controllers/score.controller.js';
+import { score21, score22, score23, score24, score26, score2} from '../controllers/score.controller.js';
 
 // Create a new score
-router.route('/grade211')
-    .post(grade211);
+router.route('/grade21')
+    .post(score21);
+
+router.route('/grade22')
+    .post(score22);
+
+router.route('/grade23')
+    .post(score23);
+
+router.route('/grade24')
+    .post(score24);
+
+router.route('/grade26')
+    .post(score26);
+
+router.route('/grade2')
+    .post(score2);
 
 export default router;
